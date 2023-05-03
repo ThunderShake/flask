4b0d579fceb18e272defbeace63915ecfc7da8a4
ThunderShake <95875721+ThunderShake@users.noreply.github.com>
2023-05-03 13:58:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -13,32 +13,30 @@ app = Flask(__name__)
 @app.route('/api/users/register', methods=['POST'])
 def create_user():
     content_type = request.headers.get('Content-Type')
-    if (content_type == 'application/json'):
-        user_table = Crud('user_')
-        users = user_table.get_all_elements()
-        json = request.json
-        user_in_bd = False
-        if all(key in json.keys() for key in ['email', 'pw', 'name']):
-            for user in users:
-                if(user['email'] == json['email']):
-                    user_in_bd = True
-            if(not user_in_bd):
-                cols, values = RoutesHelper.insert_element('user_', json.items())
-                user_holder = user_table.getElements_and_operator(cols, values)
-                user_row = user_holder[0]
-                user_id = user_row['id']
-                return make_response({
-                    'message': 'User created successfully',
-                    'user_id': user_id
-                }),201
-            else:
-                return make_response({"message": "An account with this email already exists."}), 409
+    
+    user_table = Crud('user_')
+    users = user_table.get_all_elements()
+    json = request.json
+    user_in_bd = False
+    if all(key in json.keys() for key in ['email', 'pw', 'name']):
+        for user in users:
+            if(user['email'] == json['email']):
+                user_in_bd = True
+        if(not user_in_bd):
+            cols, values = RoutesHelper.insert_element('user_', json.items())
+            user_holder = user_table.getElements_and_operator(cols, values)
+            user_row = user_holder[0]
+            user_id = user_row['id']
+            return make_response({
+                'message': 'User created successfully',
+                'user_id': user_id
+            }),201
         else:
-            message = 'Missing required fields'
-            return make_response({'error': message}), 400
+            return make_response({"message": "An account with this email already exists."}), 409
     else:
-        message = 'Content type not supported. Expected application/json'
-        return make_response({'error': message}, 415)
+        message = 'Missing required fields'
+        return make_response({'error': message}), 400
+    
 
 @app.route('/api/users/login', methods=['POST'])
 def login():
